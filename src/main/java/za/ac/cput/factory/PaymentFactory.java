a6e696ecc34a50a5c0e3d0bf7b83c8ae60bb0b78
luyolo <luyoloskoma@gmail.com>
2024-05-17 19:56:11 +0200

    error handling

---
+++ src/main/java/za/ac/cput/factory/PaymentFactory.java
@@ -0,0 +1,24 @@
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.PaymentInfo;
+import za.ac.cput.util.Helper;
+
+
+import java.util.Date;
+
+public class PaymentFactory {
+    public static Payment createPayment(String paymentId, String customerId, double amountPaid, Date paymentDate, String paymentStatus, PaymentInfo paymentInfo) {
+        if (Helper.isNullOrEmpty(paymentId) || Helper.isNullOrEmpty(customerId) || paymentDate == null || Helper.isNullOrEmpty(paymentStatus) || paymentInfo == null) {
+            return null;
+        }
+
+        return new Payment.Builder()
+                .setPaymentId(paymentId)
+                .setCustomerId(customerId)
+                .setAmountPaid(amountPaid)
+                .setPaymentDate(paymentDate)
+                .setPaymentStatus(paymentStatus)
+                .setPaymentInfo(paymentInfo)
+                .build();
+    }
+}
