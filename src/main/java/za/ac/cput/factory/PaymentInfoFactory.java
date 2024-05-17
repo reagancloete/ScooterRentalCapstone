10f580d0a4249a23544674f62117b6209dcc587a
luyolo <luyoloskoma@gmail.com>
2024-05-17 19:56:11 +0200

    error handling

---
+++ src/main/java/za/ac/cput/factory/PaymentInfoFactory.java
@@ -0,0 +1,22 @@
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.PaymentInfo;
+import za.ac.cput.util.Helper;
+
+public class PaymentInfoFactory {
+    public static PaymentInfo createPaymentInfo(String rentalDuration, String insuranceFee, String rentalDescription, String scooterId) {
+        if (Helper.isNullOrEmpty(rentalDuration) || Helper.isNullOrEmpty(insuranceFee) || Helper.isNullOrEmpty(rentalDescription) || Helper.isNullOrEmpty(scooterId)) {
+            return null;
+        }
+
+        String paymentInfoId = Helper.generateId();
+
+        return new PaymentInfo.Builder()
+                .setPaymentInfoId(paymentInfoId)
+                .setRentalDuration(rentalDuration)
+                .setInsuranceFee(insuranceFee)
+                .setRentalDescription(rentalDescription)
+                .setScooterId(scooterId)
+                .build();
+    }
+}
