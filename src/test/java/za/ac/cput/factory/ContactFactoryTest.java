80f5fba4eca7c7626a58788f3affd49af31c238b
reagancloete <220328625@mycput.ac.za>
2024-05-17 20:52:28 +0200

    Factory Test classes for Employee & Contact

---
+++ src/test/java/za/ac/cput/factory/ContactFactoryTest.java
@@ -0,0 +1,74 @@
+package za.ac.cput.factory;
+import org.junit.jupiter.api.Test;
+import za.ac.cput.domain.Contact;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContactFactoryTest
+{
+    @Test
+    void testBuildContactWithAllFields() {
+        String email = "reagan@example.com";
+        String mobile = "0765427694";
+        String workTel = "0217827469";
+
+        Contact contact = ContactFactory.buildContact(email, mobile, workTel);
+        assertNotNull(contact);
+        System.out.println(contact.toString());
+        assertEquals(email, contact.getEmail());
+        assertEquals(mobile, contact.getMobile());
+        assertEquals(workTel, contact.getWorkTel());
+    }
+
+    @Test
+    void testBuildContactWithOnlyEmail() {
+        String email = "test@gmail.com";
+
+        Contact contact = ContactFactory.buildContact(email);
+        assertNotNull(contact);
+        System.out.println(contact.toString());
+        assertEquals(email, contact.getEmail());
+        assertNull(contact.getMobile());
+        assertNull(contact.getWorkTel());
+    }
+
+    @Test
+    void testBuildContactWithInvalidEmail() {
+        String email = "invalid-email";
+        String mobile = "0765427694";
+        String workTel = "0217827469";
+
+        Contact contact = ContactFactory.buildContact(email, mobile, workTel);
+        assertNull(contact);
+        System.out.println("Contact create failed due to invalid email.");
+    }
+
+    @Test
+    void testBuildContactWithInvalidMobile() {
+        String email = "reagan@gmail";
+        String mobile = "0";
+        String workTel = "0217827469";
+        System.out.println("Contact create failed due to invalid mobile number for: " + email + "/" + workTel);
+        Contact contact = ContactFactory.buildContact(email, mobile, workTel);
+        //assertNull(contact);
+    }
+
+    @Test
+    void testBuildContactWithInvalidWorkTel() {
+        String email = "test@example.com";
+        String mobile = "0765427694";
+        String workTel = "invalid-worktel";
+
+        Contact contact = ContactFactory.buildContact(email, mobile, workTel);
+        assertNull(contact);
+        System.out.println("Contact create failed due to invalid work telephone ");
+    }
+
+    @Test
+    void testBuildContactWithNullValues() {
+        Contact contact = ContactFactory.buildContact(null, null, null);
+        assertNull(contact);
+        System.out.println("Contact create failed due to null values: " + contact);
+    }
+}
---
+++ src/test/java/za/ac/cput/factory/EmployeeFactoryTest.java
@@ -0,0 +1,23 @@
+package za.ac.cput.factory;
+import org.junit.jupiter.api.Test;
+import za.ac.cput.domain.Employee;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeFactoryTest {
+    @Test
+    void testBuildEmployee(){
+        Employee e = EmployeeFactory.buildEmployee("220328625", "Reagan", "Cloete");
+        assertNotNull(e);
+        System.out.println(e.toString());
+    }
+
+    @Test
+    void testBuildEmployeeWithFail(){
+        Employee e = EmployeeFactory.buildEmployee("", "Reagan", "Cloete");
+        assertNull(e);
+        System.out.println(e);
+    }
+
+}
